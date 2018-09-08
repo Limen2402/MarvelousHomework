9fcd285b9b6cd9682b60fc761c72c48646aec9e7
Limen2402 <Limen2402@gmail.com>
2018-09-08 21:18:21 +0300

    Подогнал под условие задачи

--- Lection1/MozolenkoVK_Roman_numerals.py
+++ Lection1/MozolenkoVK_Roman_numerals.py
@@ -1,21 +1,27 @@
-#Принимаем на консольный вход римское число, выдаем число арабское
+def roman_to_decimal(rom):
 
-dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-s_input = input()
-sum = 0
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
 
-#Если буква одна, ее и выводим, если не одна, начинаем проверять порядок - если он правильный, складываем, если неправильный - вычитаем
+    #Если буква одна, ее и выводим, если не одна, начинаем проверять порядок - если он правильный, складываем, если неправильный - вычитаем
 
-if len(s_input) == 1:
-    sum += dict[s_input]
-else:
-    for i in range(len(s_input) - 1):
-        if dict[s_input[i]] >= dict[s_input[i + 1]]:
-           sum += dict[s_input[i]]
-        else:
-           sum -= dict[s_input[i]]
-    sum += dict[s_input[i + 1]]
+    if len(rom) == 1:
+        sum += dict[rom]
+    else:
+        for i in range(len(rom) - 1):
+            if dict[rom[i]] >= dict[rom[i + 1]]:
+               sum += dict[rom[i]]
+            else:
+               sum -= dict[rom[i]]
+        sum += dict[rom[i + 1]]
 
-print(sum)
+    return sum
 
-#GIT
+##################################################
+
+
+test_pairs = [("IX", 9), ("XI", 11), ("MCCII", 1202), ("MMXVIII", 2018), ("XLIX", 49), ("LXXXIX", 89), ("XCIX", 99)]
+
+for rom, dec in test_pairs:
+    converted = roman_to_decimal(rom)
+    print(converted == dec)
