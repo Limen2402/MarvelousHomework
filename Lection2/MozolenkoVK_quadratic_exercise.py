4dca425e045f7f05116c0d3bc75d363f7ff61c17
Limen2402 <Limen2402@gmail.com>
2018-09-12 13:10:17 +0300

    работаем

--- Lection2/MozolenkoVK_quadratic_exercise.py
+++ Lection2/MozolenkoVK_quadratic_exercise.py
@@ -4,7 +4,7 @@ import cmath
 def solve_quad(b, c):
     D = cmath.sqrt(b ** 2 - 4 * c)
     x1 = (- b + D) / 2
-    x2 = (- b - D) / 2
+    x2 = (- b - D) 
     return x1, x2
 
 variants = [{'b': 4.0, 'c': 3.0}, {'b': 2.0, 'c': 1.0}, {'b': 0.5, 'c': 4.0}, {'b': 1e10, 'c': 3.0}, {'b': -1e10, 'c': 4.0}]
